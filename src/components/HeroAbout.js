c86d6d5311684add9a1b662743e3a4e863399efe
sureshk999 <85228274+sureshk999@users.noreply.github.com>
2021-08-11 09:29:26 +1000

    Update HeroAbout.js

--- src/components/HeroAbout.js
+++ src/components/HeroAbout.js
@@ -6,7 +6,7 @@ import './HeroHome.css';
 function HeroAbout() {
   return (
     <div className='hero-container'>
-      <video src='/videos/video-2.mp4' autoPlay loop muted />
+      <image src='/images/90.webp'/>
       <h1>ADVENTURE AWAITS</h1>
       <p>What are you waiting for?</p>
       <div className='hero-btns'>
